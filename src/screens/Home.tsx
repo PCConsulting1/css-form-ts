e8e0d1e8325f053e16bb179ce738ec9658082541
Gerald <codyhenry527@gmail.com>
2023-05-12 11:29:14 -0400

    Add home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+// import WelcomeBanner from '../components/Welcome'
+// import PriceList from '../components/prices/PriceList'
+// import ServiceList from '../components/services/ServiceList'
+import Form from './Form'
+
+export default function Home() {
+  return (
+    <>
+      {/* <WelcomeBanner />
+      <PriceList />
+      <ServiceList /> */}
+      <Form />
+    </>
+  )
+}
