7b63385e8e6a6ddc73050031a972d0f62e5799f0
Gerald <codyhenry527@gmail.com>
2023-06-02 13:19:12 -0400

    added questions

--- src/data/questions.data.ts
+++ src/data/questions.data.ts
@@ -1,4 +1,7 @@
 //!name field must match object field
+/**
+ * Call question like: questions.taxesFiled.question('Tom', '2021')
+ */
 export const questions = {
   firstName: { name: 'first_name', question: 'first name' },
   middleName: { name: 'middle_name', question: 'middle name' },
@@ -6,8 +9,17 @@ export const questions = {
   phoneNumber: { name: 'phone_number', question: 'phone number', variant: 'phone' },
   email: { name: 'email', question: 'email', variant: 'email' },
   birthDate: { name: 'birth_date', question: 'date of birth (MM/DD/YYYY)', variant: 'date' }, 
-  citizenship: { name: 'citizenship', question: 'citizenship status' },
+  citizenship: { name: 'citizenship', question: 'citizenship status', options: '' },
   socialSecurity: { name: 'social_security', question: 'social security' },
+  cbFinAidId: { name: 'cb_fin_aid_id', question: 'College Board Id. You will have this if you have completed the CSS Profile before' },
+  streetAddress: { name: 'street_address', question: 'street address' },
+  streetAddress2: { name: 'street_address_2', question: 'street address line 2' },
+  city: { name: 'city', question: 'city' },
+  state: { name: 'state', question: 'state' },
+  zip: { name: 'zip', question: 'zip code' },
+  taxesFiled: { name: 'is_1040_filed', question: (subject:string, year:string) =>`Did ${subject} file a federal tax return for ${year}?` },
+  : { name: '', question: '' },
+  : { name: '', question: '' },
   : { name: '', question: '' },
   : { name: '', question: '' },
 }
