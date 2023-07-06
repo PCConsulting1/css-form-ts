1b3177cd8e8da93d181f6c4c91c94d9a844e6ed3
Gerald <codyhenry527@gmail.com>
2023-07-06 13:39:03 -0400

    add general input

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,16 +1,32 @@
+import Container from 
 /**
  *
  * @param variant Type of input
  * @param name Name of input field
  * @param isRequired Needs non-blank value
  * @param defaultValue Value of input field
- * @param onChange function to be called input value changes.
- *                 will contain list of questions/pages to activate
+ * @param onChange function to be called input value changes. Will contain list of questions/pages to activate
  */
+interface Props {
+  variant?: string
+  name: string
+  isRequired?: boolean
+  defaultValue: string | boolean | number
+  onChange: any
+  options?: Array<string>
+  // auth?: Array<string> will be used for validation
+}
 export default function ({
   variant = 'basic',
   name,
   isRequired = false,
   defaultValue,
   onChange,
-})
+  options = [],
+}: Props) {
+  return { switch({ variant }) {
+    case 'select': {
+      return ()
+    }
+  } }
+}
